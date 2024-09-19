af3502b902295ee7cd524048a10e81b6af0a443a
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-09-19 19:24:11 +0200

    Add files via upload

--- Home Work 10.09.24/Home Work 10.09.24/Program.cs
+++ Home Work 10.09.24/Home Work 10.09.24/Program.cs
@@ -1,13 +1,64 @@
-﻿using Home_Work_10._09._24.Menu;
-
-namespace Home_Work_10._09._24
+﻿namespace Home_Work_10._09._24
 {
     internal class Program
     {
+        static void NewGame()
+        {
+            Console.WriteLine("The new game has begun!");
+        }
+        static void LoadGame()
+        {
+            Console.WriteLine("The game is loading");
+        }
+        static void Rules()
+        {
+            Console.WriteLine("Here are the rules of the game");
+        }
+        static void AboutAuthor()
+        {
+            Console.WriteLine("Program Author: LOLO MUKHAMMED");
+        }
+        static void Exit()
+        {
+            Console.WriteLine("Exiting the program");
+        }
+
+        delegate void Menu();
+
         static void Main(string[] args)
         {
-            MainMenu mainMenu = new MainMenu();
-            mainMenu.menu();
+            Menu[] menus = [NewGame, LoadGame, Rules, AboutAuthor, Exit];
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("1 - New game");
+                Console.WriteLine("2 - Download the game");
+                Console.WriteLine("3 - Rules");
+                Console.WriteLine("4 - About the author");
+                Console.WriteLine("0 - Exit");
+                Console.Write("Select a menu item (enter a number): ");
+
+                try
+                {
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice >= 0 && choice <= 4)
+                    {
+                        menus[choice]();
+                        if (choice == 0) exit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: введите корректное число.");
+                }
+            }
         }
     }
 }
