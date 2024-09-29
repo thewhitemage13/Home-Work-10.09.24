165b2aa35f1124500635706850b91491e7913b1a
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-09-29 13:22:36 +0200

    Add files via upload

---
+++ Home Work Group/Home Work Group/ConsoleGroupPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_Group
+{
+    internal class ConsoleGroupPrinter : IPrinter<Group>
+    {
+        public void Print(Group group)
+        {
+            Console.WriteLine(group);
+        }
+    }
+}
---
+++ Home Work Group/Home Work Group/IGroupManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_Group
+{
+    internal interface IGroupManager<T1, T2>
+    {
+        public void Expel(T1 obj1);
+
+        public void FlunkedOut(T1 obj1);
+
+        public void AddStudent(T1 obj1, T2 obj2);
+
+        public void RemoveStudent(T1 obj1, T2 obj2);
+
+        public void TransferStudent(T1 obj1, T1 obj2, T2 Obj3);
+    }
+}
