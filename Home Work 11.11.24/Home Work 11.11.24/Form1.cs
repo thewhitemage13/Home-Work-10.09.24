b54f6d0da453cda6be7f281dc42bff0b5186d969
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-11-11 13:25:32 +0100

    Add files via upload

---
+++ Home Work 11.11.24/Home Work 11.11.24/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace Home_Work_11._11._24
+{
+    public partial class Form1 : Form
+    {
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem mainItem;
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateMenuButton_Click(object sender, EventArgs e)
+        {
+            if (menuStrip == null)
+            {
+                menuStrip = new MenuStrip();
+                this.Controls.Add(menuStrip);
+
+                mainItem = new ToolStripMenuItem("1");
+                menuStrip.Items.Add(mainItem);
+
+                mainItem.Click += MenuItem_Click;
+            }
+        }
+
+        private void MenuItem_Click(object sender, EventArgs e)
+        {
+            if (sender is ToolStripMenuItem menuItem)
+            {
+                if (menuItem.DropDownItems.Count == 0)
+                {
+                    count++;
+                    ToolStripMenuItem newItem = new ToolStripMenuItem(count + "");
+                    menuItem.DropDownItems.Add(newItem);
+                    newItem.Click += MenuItem_Click;
+                }
+            }
+        }
+    }
+}
