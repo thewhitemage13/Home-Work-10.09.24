9ff99265f1652fd1897734084dc24ccda0df7ed4
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-11-13 12:07:36 +0100

    Add files via upload

---
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(69, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(634, 349);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(292, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(179, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Upload image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(69, 396);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Left";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(628, 396);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Right";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(344, 396);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 4;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,73 @@
+﻿namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private List<string> imagePaths = new List<string>();
+        private int currentIndex = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            PBSettings();
+        }
+        private void PBSettings()
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+        }
+        private void pictureBox()
+        {
+            pictureBox1 = Controls.OfType<PictureBox>().FirstOrDefault();
+            if (pictureBox1 != null && currentIndex >= 0 && currentIndex < imagePaths.Count)
+            {
+                pictureBox1.Image?.Dispose();
+                pictureBox1.Image = Image.FromFile(imagePaths[currentIndex]);
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    imagePaths.AddRange(openFileDialog.FileNames);
+                    if (currentIndex == -1 && imagePaths.Count > 0)
+                    {
+                        currentIndex = 0;
+                        pictureBox();
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                pictureBox();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (currentIndex < imagePaths.Count - 1)
+            {
+                currentIndex++;
+                pictureBox();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (currentIndex >= 0 && currentIndex < imagePaths.Count)
+            {
+                imagePaths.RemoveAt(currentIndex);
+                if (currentIndex >= imagePaths.Count) currentIndex--;
+                pictureBox();
+            }
+        }
+    }
+}
